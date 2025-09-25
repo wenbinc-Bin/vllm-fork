1c1ab0262e9efe09700c448e9656b7e252b55557
Chen, Wenbin <wenbin.chen@intel.com>
2025-09-25 04:07:06 +0000

    Add qwen3-vl qwen3-omni placeholders
    Signed-off-by: Chen, Wenbin <wenbin.chen@intel.com>

--- vllm/entrypoints/chat_utils.py
+++ vllm/entrypoints/chat_utils.py
@@ -537,6 +537,8 @@ class BaseMultiModalItemTracker(ABC, Generic[_T]):
                 return "<|vision_start|><|image_pad|><|vision_end|>"
             if model_type == "qwen2_5_omni":
                 return "<|vision_start|><|IMAGE|><|vision_end|>"
+            if model_type in ("qwen3_omni_moe", "qwen3_vl_moe", "qwen3_vl"):
+                return "<|vision_start|><|image_pad|><|vision_end|>"
             if model_type == "molmo":
                 return ""
             if model_type == "aria":
@@ -555,6 +557,8 @@ class BaseMultiModalItemTracker(ABC, Generic[_T]):
             if model_type in ("qwen2_audio", "qwen2_5_omni"):
                 return (f"Audio {current_count}: "
                         f"<|audio_bos|><|AUDIO|><|audio_eos|>")
+            if model_type == "qwen3_omni_moe":
+                return "<|audio_start|><|audio_pad|><|audio_end|>"
             if model_type == "minicpmo":
                 return "(<audio>./</audio>)"
             raise TypeError(f"Unknown model type: {model_type}")
@@ -567,6 +571,8 @@ class BaseMultiModalItemTracker(ABC, Generic[_T]):
                 return "<|vision_start|><|video_pad|><|vision_end|>"
             if model_type == "qwen2_5_omni":
                 return "<|vision_start|><|VIDEO|><|vision_end|>"
+            if model_type in ("qwen3_omni_moe", "qwen3_vl_moe", "qwen3_vl"):
+                return "<|vision_start|><|video_pad|><|vision_end|>"
             if model_type in ("minicpmo", "minicpmv"):
                 return "(<video>./</video>)"
             if model_type.startswith("llava"):
