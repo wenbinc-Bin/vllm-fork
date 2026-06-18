58b909ebbe88ce7696dfaaacd460202d0ddab119
Avery Miao <avery.miao@intel.com>
2026-06-18 03:40:03 +0000

    Change DeepSeek-V4 qnorm+rope+kv_insert to SYCL kernel path
    Signed-off-by: Avery Miao <avery.miao@intel.com>

--- vllm/models/deepseek_v4/xpu/xpu_sparse.py
+++ vllm/models/deepseek_v4/xpu/xpu_sparse.py
@@ -96,11 +96,7 @@ class DeepseekV4XPUAttention(DeepseekV4Attention):
         )
         assert swa_metadata is not None
 
-        from vllm.models.deepseek_v4.xpu.xpu_qnorm_rope_kv_fp8_insert import (
-            xpu_qnorm_rope_kv_fp8_insert,
-        )
-
-        xpu_qnorm_rope_kv_fp8_insert(
+        torch.ops._xpu_C.deepseek_qnorm_rope_kv_insert(
             q,
             kv,
             self.swa_cache_layer.kv_cache,
@@ -109,6 +105,7 @@ class DeepseekV4XPUAttention(DeepseekV4Attention):
             self.rotary_emb.cos_sin_cache,
             self.eps,
             swa_metadata.block_size,
+            "fp8_ds_mla",
         )
         return q
 
